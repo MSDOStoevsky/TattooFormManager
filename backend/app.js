8d751a4f79bf5be457edde9940b09ac8824e9dab
Dylan Lettinga <dletting@mtu.edu>
2017-08-31 17:13:30 -0400

    node enpoint setup

--- backend/app.js
+++ backend/app.js
@@ -2,56 +2,37 @@
  * Dylan Lettinga
  * 08/29/2017
  */
+const mongo = require('./DBops');
 var fs = require("fs");
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var Client = require('mongodb').MongoClient;
 
-var conn = "mongodb://localhost:27017/tattooformmgr";
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var app = express();
-  
-/*pool.query('SELECT * FROM formmgr."Invoice"', (err, res) => {
-    console.log(err, res)
-    pool.end()
-})*/
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/get/search/:lookup', function (req, res) {
     var res_obj = { "invoices" : [] };
     console.log("[200] " + req.method + " to " + req.url);
-    client.query('SELECT * FROM formmgr."Invoice" WHERE date::text LIKE \'%' + req.params.lookup +'%\'', (err, qres) => {
-        for(row = 0; row < qres.rowCount; row++)
-        {
-            res_obj.invoices[row] = qres.rows[row];
-        }
-        res.end(JSON.stringify(res_obj))
-    })
+    res.end(mongo.query("invoice", {}));
 })
 
-app.post('/post/login', urlencodedParser, function (req, res) {
+app.post('/post/login', function (req, res) {
 	console.log("[200] " + req.method + " to " + req.url);
 })
 
-app.post('/post/client', urlencodedParser, function (req, res) {
-	console.log("[200] " + req.method + " to " + req.url);
-})
-
-app.post('/post/invoice', urlencodedParser, function (req, res) {
-	console.log("[200] " + req.method + " to " + req.url);
+app.post('/post/invoice', function (req, res) {
+    console.log("[200] " + req.method + " to " + req.url);
+    console.log(req);
+    mongo.insertdoc("invoice", req.body);
+    var status = { "status": "success", "object": req.body}
+    res.end('{"status":"success"}')
 })
 
 var server = app.listen(4067, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Server started at http://%s:%s", host, port)
-
-    Client.connect(conn, function(err, db) {
-        if (err) throw err;
-        console.log("Database created/connected");
-        db.close();
-    });
+    console.log("Server started at http://%s:%s", host, port);
+    mongo.createcol("invoice");
 })
--- backend/dbops/index.js
+++ backend/dbops/index.js
@@ -1,7 +1,61 @@
 var Client = require('mongodb').MongoClient;
+var conn = "mongodb://localhost:27017/tattooformmgr";
 
-db.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-});
+module.exports = {
+    /* create collection (table) */
+    createcol: (name) => {
+        Client.connect(conn, function(err, db) {
+            if (err) throw err;
+
+            db.createCollection(name, function(err, res) {
+                if (err) throw err;
+                console.log("Collection created!");
+                db.close();
+            });
+        });
+    },
+    /* drop collection (table) */
+    dropcol: (name) => {
+        Client.connect(conn, function(err, db) {
+            if (err) throw err;
+            db.collection(name).drop(function(err, delOK) {
+                if (err) throw err;
+                if (delOK) console.log("Collection deleted");
+                db.close();
+            });
+        });
+    },
+    /* insert document (record) */
+    insertdoc: (name, insertobj) => {
+        Client.connect(conn, function(err, db) {
+            if (err) throw err;
+            db.collection(name).insertOne(insertobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            });
+        });
+    },
+    /* delete document (record) */
+    deletedoc: (name, deleteobj) => {
+        Client.connect(conn, function(err, db) {
+            if (err) throw err;
+            db.collection(name).deleteOne(deleteobj, function(err, res) {
+                if (err) throw err;
+                console.log("document deleted");
+                db.close();
+            });
+        });
+    },
+    /* query database */
+    query: (name, queryobj) => {
+        Client.connect(conn, function(err, db) {
+            if (err) throw err;
+            db.collection(name).find(queryobj).toArray(function(err, result) {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+            });
+        });
+    }
+}
